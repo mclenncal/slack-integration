62a1421632b4524d7ade379f6b4dcae504116b67
mclenncal <41250469+mclenncal@users.noreply.github.com>
2019-09-02 21:10:29 +1000

    OpenShift is not playing friendly

--- routes/slack.js
+++ routes/slack.js
@@ -1,7 +1,38 @@
 var express = require('express');
-var models = require('./models/models.js');
 var file = require('fs');
 var router = express.Router();
+
+function incomingSlackPayload(payload, authorisation) {
+    return {
+        authorized: authorized(payload, authorisation),
+        valid: validate(payload),
+        token: payload.token
+    }
+
+    function validate(payload) {
+        console.log('Validating request: ' + JSON.stringify(payload));
+        var token = payload.token;
+    
+        if(typeof(token) === 'undefined' || token === null)
+            return false;
+    
+        return true;
+    }
+    
+    function authorized(payload, authorization) {
+        console.log('Authorizing request: ' + JSON.stringify(payload));
+    
+        return payload.token === authorization.token
+            && authorization.claims.indexOf(payload.command) > -1;
+    }
+}
+
+function outgoingSlackPayload(response_url) {
+    return {
+        response_url: response_url
+    };
+
+}
 
 /*
  * Test some shit
